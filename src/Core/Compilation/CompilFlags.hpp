ae95fac508bd370906b974a46a45d82fd70c13c3
pulk66-s <hugo.marrasse@epitech.eu>
2023-09-13 18:47:34 +0200

    feat: Changes of default flags

--- src/Core/Compilation/CompilFlags.hpp
+++ src/Core/Compilation/CompilFlags.hpp
@@ -7,12 +7,12 @@ namespace Compil
 {
     struct Flags
     {
-        bool debug = false;         // Debugging flags (-g3 and -fsanitize)
-        bool optimisation = false;  // Optimisation flags
-        bool warns = true;          // Warning flags
-        bool warnError = false;     // Warn Error flag (-Werror)
-        bool defaultInclude = true; // Default include folder path
-        bool defaultName = true;    // Default binary name
+        bool debug = false;             // Debugging flags (-g3 and -fsanitize)
+        bool optimisation = false;      // Optimisation flags
+        bool warns = false;             // Warning flags
+        bool warnError = false;         // Warn Error flag (-Werror)
+        bool defaultInclude = true;     // Default include folder path
+        bool defaultName = true;        // Default binary name
     };
 }
 
